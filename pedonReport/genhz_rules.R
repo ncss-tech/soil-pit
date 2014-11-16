6c661d460307166f2abd1c7f8566298be8b6afc5
sroecker01 <stephen.roecker@gmail.com>
2014-11-16 11:21:12 -0500

    upload

---
+++ pedonReport/genhz_rules.R
@@ -0,0 +1,17 @@
+## generalized horizon designations
+gen.hz.rules <- list(
+  'carrizo'=list(
+    n=c('C', 'A', '2Bk', '2Ckq', '2Btkqb', 'Bk'),
+    p=c('C', '^A', 'BA|^Bk|^2Bk', 'C1$|C2$|^BCk|^2BCk|^Ck|^2Ck|^Cq|^2Cq', 'Bt|^2Bkqb')
+  ),
+  'cajon'=list(
+    n=c('A', 'Bk', 'Ck'),
+    p=c('^A', '^BA|^Bk|^Bw', '^C|BC')
+  )
+)
+
+summary(factor(f$genhz))
+f$genhz <- as.character(f$genhz)
+f$genhz <- ifelse(is.na(f@horizons$genhz), generalize.hz(f$hzname, gen.hz.rules$carrizo$n, gen.hz.rules$carrizo$p), f@horizons$genhz)
+f$genhz <- factor(f$genhz, levels=gen.hz.rules$carrizo$n, ordered=T) 
+summary(factor(f$genhz))
