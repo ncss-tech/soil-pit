7eab92cdfd80cff59078b6ced6c64525d0f20ee5
sroecker01 <stephen.roecker@gmail.com>
2015-01-16 18:46:07 -0500

    refiled
    refiled folders, and placed small gis .R files into example.Rmd which
    can be sorted by chunk

--- gis/acreCount.R
+++
@@ -1,3 +0,0 @@
-# Intersect ca795_int, ca795_a, and govt_ownership. Calculate Geometry. Export from ArcMap. Import to R. Run following script.
-
-test<-tapply(data$acres, list(MUSYM=data$MUSYM, AGENCY=data$AGENCY), sum, na.rm=TRUE)
--- gis/definitionQuery.R
+++
@@ -1,7 +0,0 @@
-# Definition query
-# Copy Mapunit table out of NASIS 
-setwd("C:/Users/stephen.roecker/Documents")
-test <- read.csv("mapunit.table.csv")
-mukey <- test$Rec.ID
-paste("'", mukey[1:length(mukey)-1], "'",collapse=",", sep='')
-# Paste results after the following, "MUKEY" IN ...
