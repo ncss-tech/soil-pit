08cf5ec45d2a3b00fc844b915bc228984ebe6e71
Jennifer Outcalt <jennifer.outcalt@oh.usda.gov>
2015-02-23 14:27:07 -0500

    Jennifer changes

---
+++ soilReports/genhz_rules/Sebewa_rules.R
@@ -0,0 +1,17 @@
+ghr <- list(
+  "Sebewa"=list(
+    n=c("Ap", 
+        "A", 
+        "Bg", 
+        "Btg", 
+        "2Cg",  
+        "NA"),
+    
+    p=c("Ap|AP",
+        "^A$",
+        "Bg",        
+        "^Btg|BCtg",
+        "^C|2C|BCg",
+        "NA")
+  )
+)
