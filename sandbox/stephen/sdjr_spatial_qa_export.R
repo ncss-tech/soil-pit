b0eb2f8702a6ab4193e6babb486233231ed3f144
Stephen Roecker <stephen.roecker@gmail.com>
2015-09-17 14:16:32 -0400

    sdjr_spatial_qa_export.R fix and slim

--- sandbox/stephen/sdjr_spatial_qa_export.R
+++ sandbox/stephen/sdjr_spatial_qa_export.R
@@ -1,47 +1,54 @@
 library(plyr)
 library(foreign)
 
-region <- read.csv("L:/geodata/soils/SSA_Regional_OWnership_MASTER_MLRA_OFFICE.csv")
-test <- read.table("L:/geodata/project_data/11WAV/QA_MapunitCheck_RTSD_Region_11_WAV_FY15_edited2_gdb.txt", sep = "\t", header = TRUE)
 
-test2 <- region[match(test$SurveyID, region$AREASYMBOL), ]
-test3 <- test2[test2$Region == 11, ]
+# Set parameters
 
-
-
-region <- read.csv("L:/geodata/soils/SSA_Regional_OWnership_MASTER_MLRA_OFFICE.csv")
-check <- read.dbf("L:/geodata/project_data/11FIN/mupolygon_edits_final2.dbf", as.is = TRUE)
-test <- sort(unique(check$AREASYMBOL))
-test2 <- region[match(test, region$AREASYMBOL), ]
-test3 <- test[test2$Region == 11]
+region  <- 11
+ownership <- read.csv("M:/geodata/soils/SSA_Regional_OWnership_MASTER_MLRA_OFFICE.txt")
 
 
-# Export list
-region <- read.csv("L:/geodata/soils/SSA_Regional_OWnership_MASTER_MLRA_OFFICE.csv")
 
-R11ATL <- cbind(read.dbf("L:/geodata/project_data/11ATL/mupolygon_edits_final_with_Region10.dbf", as.is = TRUE), office = "11ATL")
-R11CAR <- cbind(read.dbf("L:/geodata/project_data/11CAR/mupolygon_edits.dbf", as.is = TRUE), office = "11CAR")
-R11CLI <- cbind(read.dbf("L:/geodata/project_data/11CLI/mupolygon_edits.dbf", as.is = TRUE), office = "11CLI")
-R11GAL <- cbind(read.dbf("L:/geodata/project_data/11GAL/mupolygon_edits.dbf", as.is = TRUE), office = "11GAL")
-R11JUE <- cbind(read.dbf("L:/geodata/project_data/11JUE/mupolygon_edits_with_Region10.dbf", as.is = TRUE), office = "11JUE")
-R11SPR <- cbind(read.dbf("L:/geodata/project_data/11SPR/mupolygon_edits_with_Region10.dbf", as.is = TRUE), office = "11SPR")
-R11UNI <- cbind(read.dbf("L:/geodata/project_data/11UNI/mupolygon_edits.dbf", as.is = TRUE), office = "11UNI")
-R11WAV <- cbind(read.dbf("L:/geodata/project_data/11WAV/mupolygon_edits_with_Region10_and_Region11.dbf", as.is = TRUE), office = "11WAV")
 
-offices <- rbind(R11ATL, R11CAR, R11CLI, R11GAL, R11JUE, R11SPR, R11UNI, R11WAV)
+# Check whether QA_MapunitCheck applies to my region
 
-test <- ddply(offices, .(office, AREASYMBOL), summarize, n_musym = length(strsplit(paste0(sort(unique(MUSYM)), collapse = ", "), ", ")[[1]]), musym = paste0(sort(unique(MUSYM)), collapse = ", "), old_musym = paste0(sort(unique(ORIG_MUSYM)), collapse = ", "))
+qa_mu <- read.table("M:/geodata/project_data/11WAV/QA_MapunitCheck_RTSD_Region_11_WAV_FY15_edited_gdb.txt", sep = "\t", header = TRUE)
 
-test2 <- join(test, region, by = "AREASYMBOL")
-test3 <- subset(test2, Region == 11, select = c("office", "AREASYMBOL", "n_musym", "musym", "old_musym"))
 
-test4 <- ddply(test3, .(AREASYMBOL), summarize, offices = paste0(office, collapse = ", "))
-test5 <- join(test3, test4, by = "AREASYMBOL")
-test5 <- data.frame(test5[c(1:3)], dom = "", test5[c(6, 4, 5)], stringsAsFactors = FALSE)
+R11 <- subset(ownership, Region  == region)
+qa_mu_R11 <- qa_mu[qa_mu$SurveyID %in% R11$AREASYMBOL, ]
 
-test5[test5$offices == "11ATL, 11WAV", "dom"] <- "11WAV"
-test5[test5$offices == "11GAL, 11WAV", "dom"] <- "11WAV"
-test5[test5$AREASYMBOL == "IL015", "dom"] <- "11WAV"
-test5[test5$AREASYMBOL == "WI045", "dom"] <- "11JUE"
 
-write.csv(test5, paste0("region11_spatial_changes_", format(Sys.time(), "%Y_%m_%d"), ".csv"))
+
+
+# Summmarize export list
+
+R11ATL <- cbind(read.dbf("M:/geodata/project_data/11ATL/mupolygon_edits_final_with_Region10.dbf", as.is = TRUE), sso = "11ATL")
+R11CAR <- cbind(read.dbf("M:/geodata/project_data/11CAR/mupolygon_edits.dbf", as.is = TRUE), sso = "11CAR")
+R11CLI <- cbind(read.dbf("M:/geodata/project_data/11CLI/mupolygon_edits.dbf", as.is = TRUE), sso = "11CLI")
+R11FIN <- cbind(read.dbf("M:/geodata/project_data/11FIN/mupolygon_edits_final.dbf", as.is = TRUE), sso = "11FIN")
+R11GAL <- cbind(read.dbf("M:/geodata/project_data/11GAL/mupolygon_edits.dbf", as.is = TRUE), sso = "11GAL")
+R11IND <- cbind(read.dbf("M:/geodata/project_data/11IND/mupolygon_edits.dbf", as.is = TRUE), sso = "11IND")
+R11JUE <- cbind(read.dbf("M:/geodata/project_data/11JUE/mupolygon_edits_with_Region10.dbf", as.is = TRUE), sso = "11JUE")
+R11SPR <- cbind(read.dbf("M:/geodata/project_data/11SPR/mupolygon_edits_with_Region10.dbf", as.is = TRUE), sso = "11SPR")
+R11UNI <- cbind(read.dbf("M:/geodata/project_data/11UNI/mupolygon_edits.dbf", as.is = TRUE), sso = "11UNI")
+R11WAV <- cbind(read.dbf("M:/geodata/project_data/11WAV/mupolygon_edits_with_Region10_and_Region11.dbf", as.is = TRUE), sso = "11WAV")
+
+edits <- rbind(R11ATL, R11CAR, R11CLI, R11FIN, R11GAL, R11IND, R11JUE, R11SPR, R11UNI, R11WAV)
+edits <- edits[edits$AREASYMBOL %in% R11$AREASYMBOL, ]
+
+
+corr <- ddply(edits, .(sso, AREASYMBOL), summarize, n_musym = length(strsplit(paste0(sort(unique(MUSYM)), collapse = ", "), ", ")[[1]]), musym = paste0(sort(unique(MUSYM)), collapse = ", "), old_musym = paste0(sort(unique(ORIG_MUSYM)), collapse = ", "))
+sso_dup <- ddply(corr, .(AREASYMBOL), summarize, sso = paste0(sso, collapse = ", "))
+corr2 <- merge(corr, sso_dup, by = "AREASYMBOL")
+corr2 <- data.frame(corr2[c(2, 6, 1)], dom = "", corr2[c(3, 4, 5)], stringsAsFactors = FALSE)
+corr2$state <- substr(corr2$AREASYMBOL, 1, 2)
+corr2 <- corr2[with(corr2, order(sso.x, AREASYMBOL)), ]
+row.names(corr2) <- 1:nrow(corr2)
+
+corr2[corr2$sso == "11ATL, 11WAV", "dom"] <- "11WAV"
+corr2[corr2$sso == "11GAL, 11WAV", "dom"] <- "11WAV"
+corr2[corr2$AREASYMBOL == "IL015", "dom"] <- "11WAV"
+corr2[corr2$AREASYMBOL == "WI045", "dom"] <- "11JUE"
+
+write.csv(corr2, paste0("region11_spatial_changes_", format(Sys.time(), "%Y_%m_%d"), ".csv"))
