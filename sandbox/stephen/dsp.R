6013dd96b650f594e12e6efd78a1dae71a9cd11f
Stephen Roecker <stephen.roecker@gmail.com>
2015-11-04 21:43:45 -0500

    updates to dsp.R

--- sandbox/stephen/dsp.R
+++ sandbox/stephen/dsp.R
@@ -1,27 +1,36 @@
 library(aqp)
+library(plyr)
 library(dplyr)
 
 options(stringsAsFactors = FALSE)
 
-data <- read.csv("M:/projects/dsp/dsp_data.csv")
-site <- read.csv("M:/projects/dsp/dsp_site.csv")
-label <- read.csv("M:/projects/dsp/dsp_label.csv")
+data <- read.csv("C:/Users/Stephen/Google Drive/projects/dsp/dsp_data.csv")
+site <- read.csv("C:/Users/Stephen/Google Drive/projects/dsp/dsp_site.csv")
+label <- read.csv("C:/Users/Stephen/Google Drive/projects/dsp/dsp_label.csv")
 
-ddply(data, .(DSP.Project), summarize, n_pedons = length(unique(UserPedonID)), n_compare = length(unique(Comparison)))
-by(data, data["DSP.Project"], function(x) table(x$Comparison))
+site <- site[!names(site) %in% c("pedontype", "pedonpurpose")]
 
+names(data) <- tolower(gsub("\\.", "_", names(data)))
+names(data)[which(names(data) == "userpedonid")] <- "user_pedon_id"
+names(site) <- tolower(gsub("\\.", "_", names(site)))
+names(site)[grep("std_", names(site))] <- c("y", "x")
+names(site)[grep("pedon_lab_", names(site))] <- "lab_samp"
 
+ddply(data, .(dsp_project), summarize, n_pedons = length(unique(user_pedon_id)), n_compare = length(unique(comparison)))
+by(data, data["DSP.Project"], function(x) table(x$comparison))
 
-site$UserPedonID <- site$User.Pedon.ID
-coords <- c("Std.Longitude", "Std.Latitude")
-site_sub <- site[complete.cases(site[coords]), ]
-data_site <- join(site_sub, data, by = "UserPedonID")
-
-
-
-names(data_site)[grep("Std.", names(data_site))] <- c("y", "x")
-names(data_site) <- tolower(gsub("\\.", "_", names(data_site)))
+data_site <- join(site, data, by = "user_pedon_id")
 data_spc <- data_site
-depths(data_spc) <- UserPedonID ~ hor_top + hor_bottom
-site(data_spc) <- ~ x + y + drainagecl +
-coordinates(data_spc) <- ~ x + y
+data_spc$piid <- paste0(data_spc$user_pedon_id, "_", data_spc$user_site_id)
+depths(data_spc) <- piid ~ hor_top + hor_bottom
+site(data_spc) <- ~ user_pedon_id + user_site_id + pedon_id + lab_samp + labpedonno + name + dsp_project + kssl_project + pr_name + comparison + cond + crop + agronfeat + plot + plot_id + plot_layout + region_strata + drainagecl + earthcovkind1 + earthcovkind2 + x + y + geographic_coord_source + current_taxon_name + soil + pedon_type + pedon_purpose + taxonkind + order + suborder + great_group + subgroup + family
+
+# for some reason collectors and date won't promote to site
+# why does pedon_lab_sample__ almost equal pedonlabno, 
+# data_site[with(data_site, which(lab_samp != labpedonno)), ]
+
+data_spc1 <- subsetProfiles(data_spc, s="x != 'NA'")
+data_spc2 <- subsetProfiles(data_spc, s="x == 'NA'")
+
+coordinates(data_spc1) <- ~ x + y
+
