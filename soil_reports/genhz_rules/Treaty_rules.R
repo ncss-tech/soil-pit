89673504855103e2fe13a6b3f2a3fa505411b978
Stephen Roecker <stephen.roecker@gmail.com>
2015-12-07 16:16:16 -0500

    new rules

---
+++ soil_reports/genhz_rules/Treaty_rules.R
@@ -0,0 +1,15 @@
+ghr <- list(
+  n = c("A",
+        "Ap",
+        "Btg",
+        "2Btg",
+        "2C",
+        "2BCtg"),
+  
+  p = c("A",
+        "Ap|AP",
+        "t|T|B1|B2",
+        "2B",
+        "C",
+        "BC|CB|B3")
+  )
