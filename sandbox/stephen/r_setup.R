a63c9baae138df1ed5c6d2cfd4197ebb102abb6c
Stephen Roecker <stephen.roecker@gmail.com>
2015-12-11 14:25:50 -0500

    adding r_setup.R file, will install packages, download reports, .Rprofile

---
+++ sandbox/stephen/r_setup.R
@@ -0,0 +1,23 @@
+dir.create(path="C:/soil-pit/reports/genhz_rules", recursive = T) 
+dir.create(path="C:/R/win-library/3.2", recursive = T)
+
+
+install.packages(c("aqp", "soilDB", "RODBC", "RCurl", "XML", "circular", "colorspace", "RColorBrewer", "plyr", "ggplot2", "reshape2", "knitr", "rmarkdown", "xtable", "lattice", "maps", "sp", "gdalUtils", "raster", "'rgdal"), dependencies=TRUE)
+
+
+# Download Rprofile
+download.file("https://raw.githubusercontent.com/ncss-tech/soil-pit/master/sandbox/stephen/.Rprofile", "C:/soil-pit/.Rprofile", method = "libcurl")
+
+
+# Download latest report and rules
+download.file("https://raw.githubusercontent.com/ncss-tech/soil-pit/master/reports/pedon_summary_by_taxonname.Rmd", "C:/soil-pit/reports/pedon_summary_by_taxonname.Rmd", method = "libcurl")
+download.file("https://raw.githubusercontent.com/ncss-tech/soil-pit/master/reports/lab_summary_by_taxonname.Rmd", "C:/soil-pit/reports/lab_summary_by_taxonname.Rmd", method = "libcurl")
+download.file("https://raw.githubusercontent.com/ncss-tech/soil-pit/master/soilDB_x/utils.R", "C:/soil-pit/reports/utils.R", method = "libcurl")
+download.file("https://raw.githubusercontent.com/ncss-tech/soil-pit/master/reports/genhz_rules/genhz_rules.zip", "C:/soil-pit/reports/genhz_rules/genhz_rules.zip", method = "libcurl")
+
+download.file("https://raw.githubusercontent.com/ncss-tech/soil-pit/master/reports/mapunit_summary_by_shapefile.Rmd", "C:/soil-pit/reports/mapunit_summary_by_shapefile.Rmd", method = "libcurl") 
+download.file("https://raw.githubusercontent.com/ncss-tech/soil-pit/master/reports/mapunit_summary_by_mukey.Rmd", "C:/soil-pit/reports/mapunit_summary_by_mukey.Rmd", method = "libcurl")
+
+
+# Unzip files
+unzip(zipfile="C:/soil-pit/reports/genhz_rules/genhz_rules.zip", exdir="C:/soil-pit/reports/genhz_rules")
