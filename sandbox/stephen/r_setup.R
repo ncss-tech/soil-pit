25583b7c56bdfe14453fd7494fb08fa86a674a33
Stephen Roecker <stephen.roecker@gmail.com>
2016-02-22 13:39:12 -0500

    fixes to setup file

--- sandbox/stephen/r_setup.R
+++ sandbox/stephen/r_setup.R
@@ -1,10 +1,10 @@
-dir.create(path="C:/workspace", recursive = T)
-dir.create(path="C:/workspace/soil-pit/reports/genhz_rules", recursive = T) 
-dir.create(path="C:/workspace/soil-pit/soilDB_x", recursive = T) 
-dir.create(path="C:/R/win-library/3.2", recursive = T)
+dir.create(path="C:/workspace/soil-pit/reports/genhz_rules", recursive = TRUE) 
+dir.create(path="C:/workspace/soil-pit/soilDB_x", recursive = TRUE) 
+dir.create(path="C:/R/win-library/3.2", recursive = TRUE)
 
 x <- '.libPaths(c("C:/R/win-library/3.2", "C:/Program Files/R/R-3.2.1/library"))'
 write(x, file = "C:/workspace/.Rprofile")
+source("C:/workspace/.Rprofile")
 
 
 # Download latest report and rules
@@ -17,22 +17,22 @@ download.file("https://raw.githubusercontent.com/ncss-tech/soil-pit/master/repor
 
 
 # Unzip files
-unzip(zipfile="C:/soil-pit/reports/genhz_rules/genhz_rules.zip", exdir="C:/soil-pit/reports/genhz_rules")
+unzip(zipfile="C:/workspace/soil-pit/reports/genhz_rules/genhz_rules.zip", exdir="C:/workspace/soil-pit/reports/genhz_rules")
 
 # Install packages
-packages <- c("aqp", "soilDB", "RODBC", "RCurl", "XML", "circular", "colorspace", "RColorBrewer", "plyr", "ggplot2", "reshape2", "knitr", "rmarkdown", "xtable", "lattice", "maps", "sp", "gdalUtils", "raster", "'rgdal")
+packages <- c("aqp", "soilDB", "RODBC", "RCurl", "XML", "circular", "colorspace", "RColorBrewer", "plyr", "reshape2", "knitr", "rmarkdown", "lattice", "maps", "sp", "gdalUtils", "raster", "rgdal")
 
 local({
   r <- getOption("repos")
-  r["CRAN"] <- "http://cran.mirrors.hoobly.com/"
+  r["CRAN"] <- "http://cran.rstudio.com/"
   options(repos = r)
 })
 
 ## Install and packages and dependencies
 ipak <- function(pkg){
-  new.pkg <- pkg[!(pkg %in% installed.packages(lib.loc = "C:/R/win-library/3.2")[, "Package"])]
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
   if (length(new.pkg) > 0) 
-    install.packages(new.pkg, lib = "C:/R/win-library/3.2", dependencies = TRUE)
+    install.packages(new.pkg, dependencies = TRUE)
 }
 
 ipak(packages)
