dec358bf3c44b59c764940091bf130d7e73673ab
John Hammerly <hammerly@users.noreply.github.com>
2017-05-23 12:51:24 -0400

    Reorganized dashboard menu and submenu items.

--- sandbox/john/water_table_report_app/app.R
+++ sandbox/john/water_table_report_app/app.R
@@ -1,47 +1,57 @@
 library(shinydashboard)
 
 header<-dashboardHeader(
-  title="Region 11 SDA Web Application", titleWidth= 450)
+  title="Region 11 SDA Web Application", titleWidth= 450
+    )
 
 sidebar<-dashboardSidebar(
   sidebarMenu(
-    menuItem("Water Table Plots", tabName="WTPlots", selected=TRUE, icon=icon("area-chart")),
-    menuItem("Organic Matter Plots", tabName="OMPlots", icon=icon("line-chart"))))
+    menuItem("Water Table", selected=TRUE, icon=icon("tint"),startExpanded=TRUE,
+             menuSubItem("Plot", tabName="WTPlots", selected=TRUE, icon=icon("area-chart")),
+             menuSubItem("Data", tabName="Data", icon=icon("table")), 
+      textInput(
+        inputId="inmukey",
+        label="Enter mukey to plot -",
+        406339),actionButton("submukey", "Submit"), radioButtons(inputId="filltype", "Choose fill:", c("flooding","ponding")), br()
+),
+    menuItem("Organic Matter", tabName="OMPlots", icon=icon("leaf")),
+    menuItem("Source Code", icon=icon("file-code-o"), href="https://github.com/ncss-tech/soil-pit/blob/master/sandbox/john/water_table_report_app/app.R"),
+    menuItem("Help", tabName="help", icon=icon("question"))))
 
 body<-dashboardBody(
   tabItems(
     tabItem(tabName="WTPlots", class="active",
-            titlePanel("Water Table Plots", windowTitle = "Water Table Plots"),
-            p("This is a web application which uses R to query component month soil moisture data
-              from Soil Data Access and plots it graphically.  You will need to determine the mapunit key
-              (mukey) of the mapunit of interest to view the data on the plot tab.  You also have the option of viewing either
-              flooding frequency or ponding frequency in the plot by clicking the radio buttons below.  
-              To view the data table used to create the graphic, select the data tab."),
-            sidebarLayout(
-              sidebarPanel(width=2,
-                           
-                           textInput(
-                             inputId="inmukey",
-                             label="Enter mukey to plot -",
-                             406339), actionButton("submukey", "Submit"), br(), p(), tags$b("CURRENT SELECTION:"), br(), p(), tags$b("Mapunit Name - "), br(), uiOutput("muname", container= tags$span), br(), p(), radioButtons(inputId="filltype", "Choose fill:", c("flooding","ponding"))),
-              
-              mainPanel(tabsetPanel(
-                type="pills",
-                tabPanel("Plot",
+            titlePanel("Water Table Plots"),
+            verticalLayout(
+              mainPanel(infoBox("Mapunit Name:", uiOutput("muname", inline= TRUE, container=span), width=12, icon=icon("map"), color="blue"),
                          fluidRow(
-                           plotOutput("result")),      p("This application was developed by John Hammerly and Stephen Roecker.")
+                           box(plotOutput("result"), width=12)),      box("This application was developed by John Hammerly and Stephen Roecker.", width=12)
                          
-                ),
-                tabPanel("Data", dataTableOutput("shdatatab"),p("This application was developed by John Hammerly and Stephen Roecker."))
-              )
-              )
-            )
-            ),
-    tabItem(tabName="OMPlots", titlePanel("Organic Matter Plots", windowTitle = "Organic Matter Plots"), p("This Plot is currently under development.")
-    )
-  )
-  )
+                ))),
 
+              
+              
+            
+            
+    tabItem(tabName="Data",
+            titlePanel("Water Table Data"),
+            verticalLayout(
+              mainPanel(infoBox("Mapunit Name:", uiOutput("muname2", inline= TRUE, container=span), width=12, icon=icon("map"), color="blue"),
+                        fluidRow(
+            dataTableOutput("shdatatab"),p("This application was developed by John Hammerly and Stephen Roecker."))))),
+    tabItem(tabName="OMPlots", titlePanel("Organic Matter"), box("This Plot is currently under development.")),
+            tabItem(tabName="help", titlePanel("Help"), box("This site is a set web applications which use",
+                                                            a(href="https://www.r-project.org/", "R"), "to query information from",
+                                                            a(href="https://sdmdataaccess.nrcs.usda.gov/", "Soil Data Access"),
+                                                            "and assembles the data into a table or plots it graphically.",
+                                                             width=12),
+                    fluidRow(
+                    infoBox("Water Table", "You will need to determine the mapunit key (mukey) of the mapunit of interest to view the data tables and plots.  Enter this number in the text input box in the side bar.  You also have the option of viewing either flooding frequency or ponding frequency in the plot by clicking the radio buttons.", width=6, icon=icon("tint"), color="blue"),
+                    infoBox("Organic Matter", "This plot is currently still in development.", icon=icon("leaf"), color="green")))
+    )
+  
+            
+)
 
 ui <- dashboardPage(header, sidebar, body)
 
