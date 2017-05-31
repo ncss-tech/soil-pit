7ccfb8f0ede247f4e0abdfb0f50c023052ed047a
John Hammerly <hammerly@users.noreply.github.com>
2017-05-31 10:02:02 -0400

    Added a choice list for query methods, expanded help page.

--- sandbox/john/water_table_report_app/app.R
+++ sandbox/john/water_table_report_app/app.R
@@ -9,9 +9,10 @@ sidebar<-dashboardSidebar(
     menuItem("Water Table", selected=TRUE, icon=icon("tint"),startExpanded=TRUE,
              menuSubItem("Plot", tabName="WTPlots", selected=TRUE, icon=icon("area-chart")),
              menuSubItem("Data", tabName="Data", icon=icon("table")), 
+      selectInput("wtchoice", "Choose a query method -", c("Mapunit Key"="mukey","National Mapunit Symbol"="nationalmusym","Mapunit Name"="muname"), selected="mukey",multiple=FALSE),
       textInput(
         inputId="inmukey",
-        label="Enter mukey to plot -",
+        label="Enter query -",
         406339),
         actionButton("submukey", "Submit"),
         radioButtons(inputId="filltype", "Choose fill:", c("flooding","ponding")), br()
@@ -19,9 +20,10 @@ sidebar<-dashboardSidebar(
     menuItem("Organic Matter", icon=icon("leaf"),
              menuSubItem("Plot", tabName="OMPlots", icon=icon("area-chart")),
              menuSubItem("Data", tabName="omdata", icon=icon("table")),
+             selectInput("omchoice", "Choose a query method -", c("Mapunit Key"="mukey","National Mapunit Symbol"="nationalmusym","Mapunit Name"="muname"), selected="mukey",multiple=FALSE),
              textInput(
                inputId="ominmukey",
-               label="Enter mukey to plot -",
+               label="Enter query -",
                406338),
              actionButton("omsubmukey", "Submit"), br(),p()
              ),
@@ -66,21 +68,28 @@ body<-dashboardBody(
     ),
             tabItem(tabName="help",
                     titlePanel("Help"),
-                    box("This site is a set web applications which use",
-                      a(href="https://www.r-project.org/", "R"), "to query information from",
-                      a(href="https://sdmdataaccess.nrcs.usda.gov/", "Soil Data Access"),
-                      "and assembles the data into a table or plots it graphically.",
-                      width=12),
+                    fluidRow(
+                    infoBox("About", box("This site is a set web applications which use",
+                                                   a(href="https://www.r-project.org/", "R"), "to query information from",
+                                                   a(href="https://sdmdataaccess.nrcs.usda.gov/", "Soil Data Access"),
+                                                   "and assembles the data into a table or plots it graphically.",width=12),width=12, icon=icon("info"), color="yellow"),
+                    infoBox("How to Use", box(
+                      p("1.  Choose a data type by clicking on the corresponding menu item in the list on the sidebar to the left.  Currently there are 2 data types available to explore:  Water Table and Organic Matter."),
+                      p("2.  Choose how to view the data.  You can view either a plot or data table."),
+                      p("3.  Choose a query method.  You can query using any one of 3 methods:  Mapunit Key, National Mapunit Symbol, or Mapunit Name.  A choice list is provided."),
+                      p("4.  Enter your query.  Be sure to enter the proper format.  Each of the previously mentioned methods has a unique set of criteria in order to return data without error."),
+                      p("5.  Click the Submit button.  The query will not execute until this button is clicked."),width=12),width=12, icon=icon("life-ring"), color="purple"
+                      )),
                     fluidRow(
                       infoBox("Water Table",
-                            "First, determine the mapunit key (mukey) of the mapunit of interest to view the data tables and plots.  You can do this within Web Soil Survey by using the identify button.
-                            Enter this number in the text input box in the side bar and click on the submit button.  
-                            You also have the option of viewing either flooding frequency or ponding frequency in the plot by clicking the radio buttons.",
+                            box("Use this data to learn more about the moisture the soil.  The plot shows depths at which different moisture status typically occur throughout the year.  You also have the option of viewing either flooding frequency or ponding frequency in the plot by clicking the radio buttons.", width=12),
                             width=12, icon=icon("tint"), color="blue"),
                       infoBox("Organic Matter",
-                              "First, determine the mapunit key (mukey) of the mapunit of interest to view the data tables and plots.  You can do this within Web Soil Survey by using the identify button.
-                            Enter this number in the text input box in the side bar and click on the submit button.  No additional options are available.",
-                            icon=icon("leaf"), color="green", width=12)
+                              box("Use this data to learn more about the organic matter in the soil.  The plot shows how organic matter changes with depth.  There are no additional options for viewing this data.", width=12),
+                            icon=icon("leaf"), color="green", width=12),
+                      infoBox("Source Code",
+                              box("This menu item provides a link to a GitHub repository containing the computer code used in this application", width=12),
+                              width=12, icon=icon("file-code-o"), color="red")
                       )
                     ),
           tabItem(tabName="omdata",
@@ -111,7 +120,7 @@ server <- function(input, output){
   output$result <- renderPlot({ input$submukey
     
     
-    wtlevels <- get_cosoilmoist_from_SDA(WHERE = paste0("mukey = '", isolate(input$inmukey), "'"), duplicates = TRUE)
+    wtlevels <- get_cosoilmoist_from_SDA(WHERE = paste0(isolate(input$wtchoice),"='", isolate(input$inmukey), "'"), duplicates = TRUE)
     
     if (input$filltype=="flooding") {ggplot(wtlevels, aes(x = as.integer(month), y = dept_r, lty = status))+
         geom_rect(aes(xmin = as.integer(month), xmax = as.integer(month)+
@@ -136,31 +145,31 @@ server <- function(input, output){
         ggtitle("Water Table Levels from Component Soil Moisture Month Data")}})
   
   output$muname<-renderText({    input$submukey
-    wtlevels <- get_cosoilmoist_from_SDA(WHERE = paste0("mukey = '", isolate(input$inmukey), "'"), duplicates = TRUE)
+    wtlevels <- get_cosoilmoist_from_SDA(WHERE = paste0(isolate(input$wtchoice),"='", isolate(input$inmukey), "'"), duplicates = TRUE)
     
     wtlevels$muname[1]})
   
   output$muname2<-renderText({   input$submukey 
-    wtlevels <- get_cosoilmoist_from_SDA(WHERE = paste0("mukey = '", isolate(input$inmukey), "'"), duplicates = TRUE)
+    wtlevels <- get_cosoilmoist_from_SDA(WHERE = paste0(isolate(input$wtchoice),"='", isolate(input$inmukey), "'"), duplicates = TRUE)
     
     wtlevels$muname[1]})
   
   output$muname3<-renderText({   input$omsubmukey 
-    wtlevels <- get_cosoilmoist_from_SDA(WHERE = paste0("mukey = '", isolate(input$ominmukey), "'"), duplicates = TRUE)
+    wtlevels <- get_cosoilmoist_from_SDA(WHERE = paste0(isolate(input$omchoice),"='", isolate(input$ominmukey), "'"), duplicates = TRUE)
     
     wtlevels$muname[1]})
   
   output$muname4<-renderText({   input$omsubmukey 
-    wtlevels <- get_cosoilmoist_from_SDA(WHERE = paste0("mukey = '", isolate(input$ominmukey), "'"), duplicates = TRUE)
+    wtlevels <- get_cosoilmoist_from_SDA(WHERE = paste0(isolate(input$omchoice),"='", isolate(input$ominmukey), "'"), duplicates = TRUE)
     
     wtlevels$muname[1]})
   
   output$shdatatab<- DT::renderDataTable({input$submukey
-    wtlevels <- get_cosoilmoist_from_SDA(WHERE = paste0("mukey = '", isolate(input$inmukey), "'"), duplicates = TRUE)}, options = list(paging=FALSE))
+    wtlevels <- get_cosoilmoist_from_SDA(WHERE = paste0(isolate(input$wtchoice),"='", isolate(input$inmukey), "'"), duplicates = TRUE)}, options = list(paging=FALSE))
   
   output$omplot<-renderPlot({ input$omsubmukey
     # import soil data using the fetchSDA_component() function
-    omdata = fetchSDA_component(WHERE = paste0("mukey = '", isolate(input$ominmukey), "'"), duplicates= TRUE)
+    omdata = fetchSDA_component(WHERE = paste0(isolate(input$omchoice),"='", isolate(input$ominmukey), "'"), duplicates= TRUE)
     
     # Convert the data for plotting
     omdata_slice <- aqp::slice(omdata$spc, 0:200 ~ om_l + om_r + om_h)
@@ -178,7 +187,7 @@ server <- function(input, output){
       coord_flip()})
   
   output$omdatatab<- DT::renderDataTable({input$omsubmukey
-    omdata <- fetchSDA_component(WHERE = paste0("mukey = '", isolate(input$ominmukey), "'"), duplicates= TRUE);    omdata_slice <- aqp::slice(omdata$spc, 0:200 ~ om_l + om_r + om_h)
+    omdata <- fetchSDA_component(WHERE = paste0(isolate(input$omchoice),"='", isolate(input$ominmukey), "'"), duplicates= TRUE);    omdata_slice <- aqp::slice(omdata$spc, 0:200 ~ om_l + om_r + om_h)
     h = horizons(omdata_slice)
     h = merge(h, site(omdata$spc)[c("cokey", "compname", "comppct_r")], by = "cokey", all.x = TRUE)}, options = list(paging=FALSE))
   
