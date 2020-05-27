3b3e59089d23bd12f1acb22768230aa051aa8595
White <12001000492332@FEDIDCARD.GOV>
2020-05-27 09:52:34 -0600

    Update landscape_stratification.R

--- sandbox/dave/landscape stratification/landscape_stratification.R
+++ sandbox/dave/landscape stratification/landscape_stratification.R
@@ -13,6 +13,7 @@ library(raster)
 library(rgdal)
 library(caret)
 library(corrplot)
+library(factoextra)
 
 
 # set working directory
@@ -38,13 +39,56 @@ res(rstack30m)
 length(rstack30m)# roughly 200 million cells
 
 # generate a regular sample of points representative of the geographic region
+beginCluster()
 georegion.df <- sampleRegular(rstack30m, size = 5000000)
+endCluster()
+
+georegion.df <- na.omit(as.data.frame(georegion.df))
 
 # bring in piedmont shapefile
 pied.shp <- readOGR("piedmont_buffer.shp")
+pied.shp@proj4string
+rstack30m@crs@projargs
+
+# reproject shp to align with raster stack
+pied.shp <- spTransform(pied.shp, crs(rstack30m@crs))
+
 
 # clip to boundary
 pied.stk <- crop(rstack30m, pied.shp)
+
+# how big is it
+length(pied.stk)
+beginCluster()
+pied.df <- sampleRegular(pied.stk, size = 500000)
+endCluster()
+
+pied.df <- na.omit(as.data.frame(pied.df))
+
+
+# bring in basin shapefile
+basin.shp <- readOGR("basin_buffer.shp")
+#check projection
+basin.shp@proj4string
+rstack30m@crs
+
+basin.shp <- spTransform(basin.shp, crs(rstack30m@crs))
+basin.stk <- crop(rstack30m, basin.shp)
+length(basin.stk)
+beginCluster()
+basin.df <- sampleRegular(basin.stk, size = 500000)
+endCluster()
+basin.df <- na.omit(as.data.frame(basin.df))
+
+
+# we have 3 data.frames lets compare some of the covariates
+# subset the dataframes to a few selected covariates
+names(georegion.df)
+
+
+#compare.df <- as.data.frame(rbind(method = "georegion", georegion.df, method = "piedmont", pied.df,  method = "basin", basin.df))
+
+
 
 
 
